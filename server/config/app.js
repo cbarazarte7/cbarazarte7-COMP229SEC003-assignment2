9dce3fd2d6be7eddecbc04da914ac9fdb001b4fd
Carla Barazarte <cbarazarte7@gmail.com>
2023-02-23 13:28:26 -0500

    connect to mongodb and and create users model and route

--- server/config/app.js
+++ server/config/app.js
@@ -11,8 +11,25 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+//modules for authentication
+let session = require('express-session');
+let passport = require('passport');
+let passportLocal = require('passport-local');
+let localStrategy = passportLocal.Strategy;
+let flash = require('connect-flash');
+//database setup
+let mongoose = require('mongoose');
+let DB = require('./db');
+//point mongoose to the DB URI
+mongoose.connect(DB.URI, {useNewUrlParser:true, useUnifiedtopology:true});
+let mongoDB = mongoose.connection;
+mongoDB.on('error', console.error.bind(console, 'Connection Error:'));
+mongoDB.once('open', () => {
+  console.log('connected to MongoDB...');
+})
 
 let indexRouter = require('../routes/index');
+let usersRouter = require('../routes/users');
 
 let app = express();
 
@@ -22,12 +39,37 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../../public')));
 app.use(express.static(path.join(__dirname, '../../node_modules')));
 
+//setup express session.
+app.use(session({
+  secret: "SomeSecret",
+  saveUninitialized: false,
+  reSave: false
+}));
+
+//initialize flash to maintain the error msg.
+app.use(flash());
+
+//initialize passport.
+app.use(passport.initialize());
+app.use(passport.session());
+// passport user configuration
+//create a user model instance
+let userModel = require('../models/user');
+let User = userModel.User;
+passport.use(User.createStrategy());
+//serialize and deserialize the user info
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,7 +84,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error', {title: "Error"});
+  res.render('error', { title: "Error" });
 });
 
 module.exports = app;
