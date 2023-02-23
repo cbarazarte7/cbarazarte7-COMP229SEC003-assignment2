f6347cb6edac50aa2d363422f1f4ca9dd18bb600
Carla Barazarte <cbarazarte7@gmail.com>
2023-02-23 13:30:03 -0500

    add login and register logic

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,6 +7,7 @@
 
 var express = require('express');
 var router = express.Router();
+let indexController = require('../controllers/index');
 
 const projectsList = [
   {
@@ -108,5 +109,21 @@ router.get('/contact', (req, res) => {
       title: "Contact Me"
   })
 })
+
+/*GET Route for displaying the Login page*/
+router.get('/login', indexController.displayLoginPage);
+
+/*POST Route for processing the Login Page*/
+
+router.post('/login', indexController.processLoginPage);
+
+/*GET Route for register page*/
+router.get('/register', indexController.displayRegisterPage);
+
+/*POST Route for processing the Register page*/
+router.post('/register', indexController.processRegisterPage);
+
+/*GET to perform userLogout*/
+router.get('/logout', indexController.performLogout);
 
 module.exports = router;
