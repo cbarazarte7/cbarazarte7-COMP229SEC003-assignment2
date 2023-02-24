f5e55c0e854a02676622c43e388928c1f21fdcdb
Carla Barazarte <cbarazarte7@gmail.com>
2023-02-24 11:51:48 -0500

    change mongodb url to connect to azure server

--- server/config/db.js
+++ server/config/db.js
@@ -7,5 +7,5 @@
 // Connection to mongo and database
 module.exports =
 {
-    "URI":"mongodb://127.0.0.1/portfolio"
+    "URI":"mongodb://carlabarazarte-portfolio:FChmYuKgexpzpfXCYWymgTrQW6NfEwDVBy4uZskv6LVPRgVhTTMmizVjTejoMgpr9nCkvzLuk3fGACDb11EXYw%3D%3D@carlabarazarte-portfolio.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@carlabarazarte-portfolio@/portfolio"
 }
